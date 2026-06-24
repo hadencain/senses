a173f0f1d3c3d30004d5082baf2eadb6c7a27818
Haden Cain <haden.cain@gmail.com>
2026-06-24 15:52:09 -0400

    Larsen: assemble mode

---
+++ src/modes/larsen/index.js
@@ -0,0 +1,6 @@
+import { manifest } from './manifest'
+import { extractFeatures } from './processor'
+import { createSynth } from './synth'
+import { makePicture } from './render'
+
+export const mode = { manifest, extractFeatures, createSynth, makePicture }
